f9bf40aa5a37fd23e96217717b17819c484cfd23
aki2314 <akihisa.11h@gmail.com>
2022-05-29 17:20:48 +0900

    提出後修正１

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user!, except: [:show]
+  before_action :move_to_index, only: [:edit]
   def show
     @user = User.find(params[:id])
   end
